f1e3cb3143a6c415056031142c67ab24c63e21c5
Malika Bakhtiyorova <malikabakhtiyorova@yandex.ru>
2021-04-30 16:27:10 +0500

    Add login route

--- server.js
+++ server.js
@@ -1,13 +1,26 @@
 const express = require('express');
+const cookieParser = require('cookie-parser');
+const ejs = require('ejs');
 
 const app = express();
+app.use(express.json());
+app.use(cookieParser())
 
-const PORT = process.env.PORT || 3080
+const PORT = process.env.PORT || 5000
 
 app.set('view engine', 'ejs')
 
 app.get('/', (req,res) => {
     res.render('index')
+})
+
+app.get('/login', (req,res) => {
+    res.render('login')
+})
+
+app.post('/login', (req,res) => {
+    let token = req.body.token
+    console.log(token)
 })
 
 app.listen(PORT, console.log(PORT))
